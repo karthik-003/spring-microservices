3a5b57bc3c4a200aa64aa55c8cde6734710ae381
Karthik Viswanadha <kviswanadha@altimetrik.com>
2021-09-15 11:04:13 +0530

    Initial Commit

---
+++ review-service/src/test/java/com/karvis/review/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karvis.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
